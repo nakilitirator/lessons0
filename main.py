09f13d79cc50ac9088d69bf822f23c8e4b9c31f0
Leavap <pahagolik@mail.ru>
2024-08-07 18:34:29 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st program
+print(2*2+2)
+print(2*(2+2))
+print(6 == 8)
+
+#4st program
+print('123.456')
+print(123.456)
+print(123.456 * 10)
+print(int(123.456 * 10))
+print(1234 % 10)
+
+print((int(float('123.456')*10))%10)
